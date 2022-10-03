fc90083ea78edf2a028eea5f0ecfd60a2394fc16
npxtune <timhorochowik@icloud.com>
2022-10-03 22:49:59 +0200

    fixed formatting

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,23 @@ fn main() {
 
         let array_value = v[0]; // first value of array for comparison
 
-        let mut comparison: u32 = match array_value.trim().parse() {    // convert first value to int
+        let mut comparison: u32 = match array_value.trim().parse() {
+            // convert first value to int
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        for index in 1..v.len() {   //array loop to check for highest number
+        for index in 1..v.len() {
+            //array loop to check for highest number
 
-            let array_index_value: u32 = match v[index].trim().parse() {    // convert value of "x" index to int for comparison
+            let array_index_value: u32 = match v[index].trim().parse() {
+                // convert value of "x" index to int for comparison
                 Ok(num) => num,
                 Err(_) => continue,
             };
 
-            if array_index_value > comparison {     // compare and if value bigger replace
+            if array_index_value > comparison {
+                // compare and if value bigger replace
                 comparison = array_index_value;
             }
             //println!("index: {}, value: {}", index, v[index]);  // debug shit ehe
